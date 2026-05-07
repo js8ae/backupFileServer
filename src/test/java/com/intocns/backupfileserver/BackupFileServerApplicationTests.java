34be03a3dd868392e17e5531906d1e16632f623e
jin-seop <bjs@intocns.com>
2026-05-07 12:10:02 +0900

    git init

---
+++ src/test/java/com/intocns/backupfileserver/BackupFileServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intocns.backupfileserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackupFileServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
